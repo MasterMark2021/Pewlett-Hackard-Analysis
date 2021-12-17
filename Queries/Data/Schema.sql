9967a86f62a71f8244b39abd6621a5f9c9adcc64
Mark O <markokoli85@gmail.com>
2021-12-16 19:41:42 -0500

    updated

--- Queries/Data/Schema.sql
+++ Queries/Data/Schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE dept_emp (
   dept_no VARCHAR(4) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  EY FOREIGN K(emp_no) REFERENCES employees (emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   PRIMARY KEY (emp_no, dept_no)
 );
@@ -54,6 +54,6 @@ CREATE TABLE titles (
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+ FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 );
