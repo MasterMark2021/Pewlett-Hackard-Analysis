52211206bbf3321337d5711f39983100fb6bbbb7
Mark O <markokoli85@gmail.com>
2021-12-19 09:45:07 -0500

    updated queries

--- Queries/Data/queries.sql
+++ Queries/Data/queries.sql
@@ -26,3 +26,63 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+Drop Table retirement_info
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+    retirement_info.last_name,
+    dept_emp.to_date
+	from retirement_info left join dept_emp
+	on retirement_info.emp_no = dept_emp.emp_no;
+
+    -- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT a.emp_no,
+    a.first_name,
+    a.last_name,
+    b.to_date
+	from retirement_info as a  left join dept_emp as b 
+	on a.emp_no = b.emp_no;
+	
+	
+	SELECT a.emp_no,
+    a.first_name,
+    a.last_name,
+b.to_date
+INTO current_emp
+FROM retirement_info as a
+LEFT JOIN dept_emp as b
+ON a.emp_no = b.emp_no
+WHERE b.to_date = ('9999-01-01');
+
+select * from current_emp
+	
